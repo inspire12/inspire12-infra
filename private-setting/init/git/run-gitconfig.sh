aeef486fc3b1255cf7905562584ae82162d32cb9
inspire12 <inspire12@ncsoft.com>
2024-09-30 17:56:30 +0900

    스크립트 정정

--- private-setting/init/git/run-gitconfig.sh
+++ private-setting/init/git/run-gitconfig.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+set -e  # 명령어가 실패할 경우 스크립트를 즉시 종료
+set -x  # 스크립트가 실행되는 명령어를 출력
+
 echo "#### Before git config ####"
 # 색상 출력 함수 정의
 echo_info() {
@@ -8,27 +12,26 @@ echo_error() {
     echo -e "\033[1;31m[ERROR]\033[0m $1" >&2
 }
 
+get_os() {
+  OS="$(uname -s)"
+  case "$OS" in
+      Linux*)
+        echo "Linux"
+        ;;
+      Darwin*)
+        echo "macOS"
+        ;;
+      CYGWIN*|MINGW*)
+        echo "Windows"  # Git Bash, Cygwin, MinGW 환경일 때 Windows로 처리
+        ;;
+      *)
+        echo "Unknown OS: $OS"
+        exit 1
+        ;;
+  esac
+}
 
-OS="$(uname -s)"
-
-case "$OS" in
-    Linux*)
-      echo "Linux"
-      global_option="--global"
-      ;;
-    Darwin*)
-      echo "macOS"
-      global_option="--global"
-      ;;
-    CYGWIN*|MINGW*)
-      echo "Windows"  # Git Bash, Cygwin, MinGW 환경일 때 Windows로 처리
-      global_option="--global"
-      ;;
-    *)
-      echo "Unknown OS: $OS"
-      exit 1
-      ;;
-esac
+get_os
 
 # Git 설치 여부 확인
 if ! command -v git &> /dev/null
@@ -45,6 +48,10 @@ git config --global branch.diff.algorithm histogram
 git config --global init.defaultBranch main
 git config --global rebase.autostash true
 
+if [ $? -ne 0 ]; then
+    echo_error "설정 실패"
+    exit 1
+fi
 
 echo_info "Git 전역 설정이 성공적으로 적용되었습니다."
 git config --global --list | grep -E 'merge.conflictstyle|pull.rebase|branch.sort|branch.diff.algorithm|init.defaultBranch|rebase.autostash'

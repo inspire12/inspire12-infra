68ede31c7a5c583de3d37b5800ddb68755cb8574
inspire12 <ox4443@naver.com>
2025-04-27 11:28:30 +0900

    feat(develop): 성능 테스트 추가

---
+++ deployment/performance-test/performance-testing-architecture/spring-order/src/main/java/com/order/perf/controller/OrderController.java
@@ -0,0 +1,35 @@
+package com.order.perf.controller;
+
+import com.order.perf.dto.response.OrderDetailsResponse;
+import com.order.perf.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @GetMapping(path = "/{orderId}")
+    public OrderDetailsResponse getOrderDetails(@PathVariable Long orderId) {
+
+        return orderService.getOrderDetailsResponse(orderId);
+    }
+
+    @GetMapping(path = "/async/{orderId}")
+    public OrderDetailsResponse getOrderDetailsAsync(@PathVariable Long orderId) {
+
+        return orderService.getOrderDetailsResponseByAsync(orderId);
+    }
+
+    @GetMapping(path = "/join/{orderId}")
+    public OrderDetailsResponse getOrderDetailsJoin(@PathVariable Long orderId) {
+
+        return orderService.getOrderDetailsResponseByJoin(orderId);
+    }
+}
---
+++ setting/setting-private/mac/README.md
@@ -0,0 +1,9 @@
+
+### 날짜 데이터 확인하는 방법
+```
+date
+date -v -1H +%s 
+```
+
+### 
+[환경변수 관리팁](https://siosio3103.medium.com/%EB%B2%88%EC%97%AD-%ED%99%98%EA%B2%BD%EB%B3%80%EC%88%98%EC%97%90-%EC%8B%9C%ED%81%AC%EB%A6%BF-%EA%B0%92%EC%9D%84-%EC%A0%80%EC%9E%A5%ED%95%98%EC%A7%80-%EC%95%8A%EA%B3%A0-%EC%82%AC%EC%9A%A9%ED%95%98%EB%8A%94-%EB%8D%94-%EB%82%98%EC%9D%80-%EB%B0%A9%EB%B2%95-2492274c544a)
